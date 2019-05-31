d79fc7d2031b18d24979a07259de76454441e6a5
DahliaEnterprise <dahliacommunication67@gmail.com>
2019-05-31 12:39:23 -0700

    Updated install order

--- delegate-install-order.sh
+++ delegate-install-order.sh
@@ -10,8 +10,25 @@ filetype=".txt"
 for buildOrderItem in "${buildOrderItems[@]}";do
 	trimmedBuildOrderItem=$(echo $buildOrderItem | xargs)
 	buildOrderItemPrerequisite="${debianPackageDirectoryLocation}${trimmedBuildOrderItem}${filetype}"
-	#echo $buildOrderItemPrerequisite	
-	line=$(head -n 1 $buildOrderItemPrerequisite)
-	echo $line
+	echo $buildOrderItemPrerequisite
+
+	#Determine Level One Prerequisite
+	readarray levelOnePrerequisites < $buildOrderItemPrerequisite
+	for levelOnePrerequisite in "${levelOnePrerequisites[@]}";do
+		echo " "$levelOnePrerequisite
+		
+		#Determine Level Two Prerequisite
+		trimmedLevelOnePrerequisites=$(echo $levelOnePrerequisite | xargs)
+		buildOrderItemPrerequisiteLevelTwo="${debianPackageDirectoryLocation}${trimmedLevelOnePrerequisites}${filetype}"
+		readarray levelTwoPrerequisites < $buildOrderItemPrerequisiteLevelTwo
+		for levelTwoPrerequisite in "${levelTwoPrerequisites[@]}";do
+			echo "  "$levelTwoPrerequisite
+		done
+
+	done
+
+	
+	#levelOnePrerequisite=$(head -n 1 $buildOrderItemPrerequisite)
+	#echo $levelOnePrerequisite
 done 
 
