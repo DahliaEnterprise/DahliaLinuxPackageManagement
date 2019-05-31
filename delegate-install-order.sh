0d73a9385341a63fc357d61417a97027f0764609
DahliaEnterprise <dahliacommunication67@gmail.com>
2019-05-30 20:19:07 -0700

    Updated install order

--- delegate-install-order.sh
+++ delegate-install-order.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
-debianPackageDirectoryLocation="/home/dahlia/Downloads/DahliaLinuxPackageManagement/Base/Debian/9.9.0/BuildEssential/amd64/DownloadSource/*"
+debianPackageDirectoryLocation="/home/dahlia/Downloads/DahliaLinuxPackageManagement/Base/Debian/9.9.0/BuildEssential/amd64/"
+entryBuildOrder="BuildEssential_amd64.txt"
+
+fileDirectoryEntryBuildOrder=$debianPackageDirectoryLocation$entryBuildOrder
+
+#Entry Build Order Loop
+readarray buildOrderItem < $fileDirectoryEntryBuildOrder                                         
+
+for row in "${buildOrderItem[@]}";do                                                      
+  row_array=(${row})                                                            
+  first=${row_array[0]}                                                         
+  echo ${first}                                                                 
+done 
+
